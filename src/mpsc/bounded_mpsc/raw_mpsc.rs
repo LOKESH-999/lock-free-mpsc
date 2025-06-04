a0b6c05a82035584eed52919945f53fdb94471b0
loki-anonymous <lokesh.nachimuthu@codezeros.com>
2025-06-04 17:42:05 +0530

    global-backoff

--- src/mpsc/bounded_mpsc/raw_mpsc.rs
+++ src/mpsc/bounded_mpsc/raw_mpsc.rs
@@ -1,85 +1,41 @@
-use std::{
-    fmt::Debug,
-    hint::spin_loop,
-    mem::transmute,
-    sync::atomic::{AtomicU32, AtomicUsize},
-};
+use std::{fmt::Debug, hint::spin_loop, mem::transmute, sync::atomic::{AtomicU32, AtomicUsize}};
 
 use crate::cache_padded::CachePadded;
 
 use super::slot_arr::SlotArr;
-use std::sync::atomic::Ordering::{AcqRel, Acquire, Release};
+use std::sync::atomic::Ordering::{Release,Acquire,AcqRel};
 
-const MAX_WAIT_SPIN: u32 = 1 << 18;
-const MIN_WAIT_SPIN: u32 = 32;
-struct GlobalBackoff {
-    active_threads: AtomicUsize,
+
+pub struct RawMpsc<T>{
+    next_head:CachePadded<AtomicUsize>,
+    tail:CachePadded<AtomicUsize>,
+    slots:SlotArr<T>
 }
 
-impl GlobalBackoff {
-    pub(crate) fn new() -> Self {
-        GlobalBackoff {
-            active_threads: 0.into(),
-        }
-    }
-
-    #[inline(always)]
-    pub(crate) fn reg_wait(&self) {
-        let n_iters = self.active_threads.fetch_add(1, AcqRel);
-        self.wait_with_spin(n_iters as u32);
-    }
-
-    #[inline(always)]
-    pub(crate) fn wait(&self) {
-        let n_iters = (MIN_WAIT_SPIN << self.active_threads.load(Acquire)).min(MAX_WAIT_SPIN);
-        self.wait_with_spin(n_iters);
-    }
-
-    #[inline(always)]
-    pub(crate) fn wait_with_spin(&self, iters: u32) {
-        for _ in 0..iters {
-            spin_loop();
-        }
-    }
-    #[inline(always)]
-    pub(crate) fn de_reg(&self) {
-        self.active_threads.fetch_sub(1, AcqRel);
-    }
-}
-
-pub struct RawMpsc<T> {
-    next_head: CachePadded<AtomicUsize>,
-    tail: CachePadded<AtomicUsize>,
-    slots: SlotArr<T>,
-}
-
-impl<T: Debug> RawMpsc<T> {
-    pub fn new(capacity: usize) -> Self {
+impl<T:Debug> RawMpsc<T>{
+    
+    pub fn new(capacity:usize)->Self{
         let slots = SlotArr::new(capacity);
         let next_head = CachePadded::new(0.into());
         let tail = CachePadded::new(0.into());
-        Self {
+        Self{
             next_head,
             tail,
-            slots,
+            slots
         }
     }
-    pub fn push(&self, data: T) -> Result<(), T> {
+    pub fn push(&self,data:T)->Result<(),T>{
         let mut curr_head = self.next_head.load(Acquire);
         loop {
-            let next_head = curr_head + 1;
-            let is_less =
-                unsafe { transmute::<isize, usize>(-((next_head < self.slots.capacity) as isize)) };
+            let next_head = curr_head + 1 ;
+            let is_less = unsafe { transmute::<isize,usize>(-((next_head < self.slots.capacity) as isize)) };
             let next_head_bounded = next_head & is_less;
-            if next_head_bounded != self.tail.load(Acquire) {
-                match self
-                    .next_head
-                    .compare_exchange(curr_head, next_head_bounded, AcqRel, Acquire)
-                {
-                    Err(h) => curr_head = h,
-                    Ok(_) => break,
+            if next_head_bounded != self.tail.load(Acquire){
+                match self.next_head.compare_exchange(curr_head, next_head_bounded, AcqRel, Acquire){
+                    Err(h) =>curr_head = h,
+                    Ok(_)=>break,
                 }
-            } else {
+            }else {
                 return Err(data);
             }
         }
@@ -87,4 +43,5 @@ impl<T: Debug> RawMpsc<T> {
         self.slots.set(curr_head, data).unwrap();
         Ok(())
     }
+
 }
